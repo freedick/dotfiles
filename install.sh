73fc5e04f2ef38a10fedfd3857e29479b47a56b6
Fredrik Nordin <freedick@bender.campus.ltu.se>
2014-09-05 15:19:36 +0200

    Add dotfile filter ability in install script.

--- install.sh
+++ install.sh
@@ -1,9 +1,28 @@
 #!/bin/bash
 
-files="xinitrc vimrc vim Xresources config/openbox"
+#Define files
+xfiles="xinitrc Xresources config/openbox"
+vimfiles="vimrc vim"
+otherfiles=""
+
+#Filter out unwanted files
+for i; do
+  case "$i" in
+    --nox  ) xfiles=""
+          shift ;;
+    --novim  ) vimfiles=""
+          shift ;;
+    --help  ) echo $helptext; exit ;;
+  esac
+done
+
+#Remove old backup and back up
 rm -r ./backup
 mkdir ./backup
 
+files="$vimfiles $xfiles $otherfiles"
+
+#Install dotfiles and backup old files
 for file in $files; do
 	mv ~/.$file ./backup 
 	ln -s `pwd`/$file ~/.$file
