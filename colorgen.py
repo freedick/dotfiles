d2d9d4ab4516b43c731b0ef789ac0a5eb47116ab
Fredrik <freddan007@gmail.com>
2016-04-03 01:01:48 +0200

    fix hash function

--- colorgen.py
+++ colorgen.py
@@ -4,7 +4,7 @@ def myhash(s):
 	if (len(s)==0):
 		 return 31337
 	wat = s[0]
-	return ((ord(wat)<<7)+ord(wat)<<3+(myhash(s[1:])>>5))&(0xffffffff)
+	return ((ord(wat)<<7)+(+ord(wat)<<3)-(ord(wat)-1)+(myhash(s[1:])>>5))&(0xffffffff)
 color = (myhash(argv[1]))%16
 color_upper = int(color/8)
 color_lower = int(color%8)
