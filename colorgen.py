16f880dd0ce9640f3889a6708bab0a593a5a96f8
Fredrik Nordin <freedick@bender.campus.ltu.se>
2016-04-05 14:44:21 +0200

    improve hash function

--- colorgen.py
+++ colorgen.py
@@ -2,9 +2,13 @@
 from sys import argv, stdout
 def myhash(s):
 	if (len(s)==0):
-		 return 31337
+		 return 13371337
+	inner_hash = myhash(s[1:])
+
 	wat = s[0]
-	return ((ord(wat)<<7)+(+ord(wat)<<3)-(ord(wat)-1)+(myhash(s[1:])>>5))&(0xffffffff)
+	return ((ord(wat)<<31)+(ord(wat)<<3)-(ord(wat)+1)+(inner_hash<<5)+(inner_hash>>5))&(0xffffffff)
+if len(argv)<2:
+    argv.append("test")
 color = (myhash(argv[1]))%16
 color_upper = int(color/8)
 color_lower = int(color%8)
